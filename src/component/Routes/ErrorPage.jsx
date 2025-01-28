9a3581ebe302efbd7838ede09af4094f9ae838ef
Bright Anyawe <brightanyawe55@gmail.com>
2025-01-28 19:55:24 +0000

    Style the Errorpage

--- src/component/Routes/ErrorPage.jsx
+++ src/component/Routes/ErrorPage.jsx
@@ -1,13 +1,48 @@
 import { Link } from "react-router-dom";
+import { Button, Box, Typography, Container } from "@mui/material";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
-    </div>
+    <Container
+      component="main"
+      maxWidth="lg"
+      className="min-h-screen flex flex-col items-center justify-center bg-gray-100"
+    >
+      <Box
+        sx={{
+          textAlign: "center",
+          py: 8,
+          px: 4,
+          backgroundColor: "white",
+          boxShadow: 3,
+          borderRadius: 2,
+        }}
+        className="w-full max-w-xl"
+      >
+        <Typography variant="h2" className="text-gray-800 font-bold mb-4">
+          404
+        </Typography>
+        <Typography variant="h5" className="text-gray-600 mb-6">
+          Oh no, this route doesn&apos;t exist!
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          component={Link}
+          to="/"
+          className="bg-blue-500 hover:bg-blue-600 text-white"
+          sx={{
+            textTransform: "none",
+            borderRadius: 2,
+            py: 1.5,
+            px: 4,
+          }}
+        >
+          Go Back to Home
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
